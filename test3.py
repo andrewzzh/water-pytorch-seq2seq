f4b9632c2fbb6c2ad2810c3a8540c5f2f0c4f22f
Andrew Zhang <andrew_zzh@hotmail.com>
2025-12-23 20:09:29 -0500

    2025-12-23-01

---
+++ test3.py
@@ -0,0 +1,34 @@
+import os
+import tokenizers
+
+if os.path.exists("en_tokenizer.json") and os.path.exists("fr_tokenizer.json"):
+    en_tokenizer = tokenizers.Tokenizer.from_file("en_tokenizer.json")
+    fr_tokenizer = tokenizers.Tokenizer.from_file("fr_tokenizer.json")
+else:
+    en_tokenizer = tokenizers.Tokenizer(tokenizers.models.BPE())
+    fr_tokenizer = tokenizers.Tokenizer(tokenizers.models.BPE())
+
+    # Configure pre-tokenizer to split on whitespace and punctuation, add space at beginning of the sentence
+    en_tokenizer.pre_tokenizer = tokenizers.pre_tokenizers.ByteLevel(add_prefix_space=True)
+    fr_tokenizer.pre_tokenizer = tokenizers.pre_tokenizers.ByteLevel(add_prefix_space=True)
+
+    # Configure decoder: So that word boundary symbol "Ġ" will be removed
+    en_tokenizer.decoder = tokenizers.decoders.ByteLevel()
+    fr_tokenizer.decoder = tokenizers.decoders.ByteLevel()
+
+    # Train BPE for English and French using the same trainer
+    VOCAB_SIZE = 8000
+    trainer = tokenizers.trainers.BpeTrainer(
+        vocab_size=VOCAB_SIZE,
+        special_tokens=["[start]", "[end]", "[pad]"],
+        show_progress=True
+    )
+    en_tokenizer.train_from_iterator([x[0] for x in text_pairs], trainer=trainer)
+    fr_tokenizer.train_from_iterator([x[1] for x in text_pairs], trainer=trainer)
+
+    en_tokenizer.enable_padding(pad_id=en_tokenizer.token_to_id("[pad]"), pad_token="[pad]")
+    fr_tokenizer.enable_padding(pad_id=fr_tokenizer.token_to_id("[pad]"), pad_token="[pad]")
+
+    # Save the trained tokenizers
+    en_tokenizer.save("en_tokenizer.json", pretty=True)
+    fr_tokenizer.save("fr_tokenizer.json", pretty=True)
